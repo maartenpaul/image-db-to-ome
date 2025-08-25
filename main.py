caba4302f5b6ceb18c0a9f0f7df6e1a8f8a81f73
Joost de Folter <folterj@gmail.com>
2025-08-25 16:28:43 +0200

    Modified command line arguments in line with container standard names

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from converter import convert, init_logging
 
 parser = argparse.ArgumentParser(description='Convert file to ome format')
 parser.add_argument('--inputfile', required=True, help='input file')
-parser.add_argument('--output_folder', required=True, help='output folder')
-parser.add_argument('--alt_output_folder', help='alternative output folder')
-parser.add_argument('--output_format', help='output format version', default='omezarr2')
+parser.add_argument('--outputfolder', required=True, help='output folder')
+parser.add_argument('--altoutputfolder', help='alternative output folder')
+parser.add_argument('--outputformat', help='output format version', default='omezarr2')
 parser.add_argument('--show_progress', action='store_true')
 parser.add_argument('--verbose', action='store_true')
 args = parser.parse_args()
@@ -17,9 +17,9 @@ init_logging('db_to_zarr.log')
 
 result = convert(
     args.inputfile,
-    args.output_folder,
-    alt_output_folder = args.alt_output_folder,
-    output_format = args.output_format,
+    args.outputfolder,
+    alt_output_folder = args.altoutputfolder,
+    output_format = args.outputformat,
     show_progress = args.show_progress,
     verbose = args.verbose
 )
